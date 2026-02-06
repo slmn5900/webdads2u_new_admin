0e7e4ac8fa0df97081b70272fa5895d0a2f532e3
Prakash-WebDad <prakash.s@webdads2u.com>
2026-02-06 17:57:03 +0530

    build error solve

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ProtectedLayout from "./protectedRoute/ProtectedRoute";
-import Login from "./pages/Login/Login";
 import Homepage from "./pages/Home/HomePage";
 import NotFound from "./pages/NotFound/NotFound";
 import OpenPostions from "./pages/OpenPostions/OpenPostions";
@@ -10,7 +9,6 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/login" element={<Login />} />
         <Route element={<ProtectedLayout />}>
           <Route path="/" element={<Homepage />} />
           <Route path="/open-positions" element={<OpenPostions />} />
